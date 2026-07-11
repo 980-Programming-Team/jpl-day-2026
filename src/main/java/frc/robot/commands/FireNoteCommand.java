2df575ca02422bdb5feadc7a339e6a239e1a28fd
Ash_ioL <ashwathn.arasurajsankar@gmail.com>
2026-07-11 11:29:17 -0700

    finished outtake command

--- src/main/java/frc/robot/commands/FireNoteCommand.java
+++ src/main/java/frc/robot/commands/FireNoteCommand.java
@@ -1,5 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
-public class FireNoteCommand {
-    
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+public class FireNoteCommand extends Command {
+  private final Shooter shooter;
+  
+  /** Creates a new FireNote. */
+  public FireNoteCommand(Shooter shooter) {
+    this.shooter = shooter;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      shooter.fireNote(2000 , 4000);
+ }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
 }
